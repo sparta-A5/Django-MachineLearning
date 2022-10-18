2709e0e1e2de763b1231a8e09d20f2e69bd62041
naerumii <github>
2022-10-18 10:08:08 +0900

    [추가] 로그인 기능 구현 #3
    로그인 기능 추가
    1. 로그인 페이지에서 회원가입 된 유저가 아닐 경우 오류 메세지 출력
    2. 로그아웃 기능 추가
    3. 로그인 된 사용자만 메인화면으로 이동할 수 있도록 구현

--- corn_ma/settings.py
+++ corn_ma/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'corn_ma.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
