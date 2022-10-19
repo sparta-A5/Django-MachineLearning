f0a76e6fb9b7109db055a350d31966cc93cd897a
jjiwon22 <ssenzool20gmail.com>
2022-10-19 15:01:52 +0900

    image upload

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
+def img_list(request):
+    if request.method == "GET":
+        image = Image.objects.all()
+        return render(request, "tweet/upload-file.html", {'image': image})
+    if request.method == "POST":
+
+        fileTitle = request.POST["fileTitle"]
+        uploadedFile = request.FILES["uploadedFile"]
+
+        document = Image(
+            title=fileTitle,
+            image=uploadedFile,
+        )
+        document.save()
+        documents = Image.objects.all()
+
+        return render(request, "tweet/upload-file.html",context = {
+        "files": documents
+    })
