74666020c32a3d0402fe34ec4c24431868fab809
jaewon <jaewoniik94@gmail.com>
2022-10-19 23:54:27 +0900

    feat : 입력 이미지 예측 기능 추가
    - run_cat.py : 모델 로드 후 입력 이미지 예측 결과 출력 코드 추가
    - upload/upload : 입력 이미지 폴더 경로, 0번째 사진 예측 가능
    - result.png : 예측 결과 출력(사진+label)
    Close #4

--- ML_cat.py
+++ ML_cat.py
@@ -12,9 +12,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import OneHotEncoder
 
 from pprint import pprint
 
