08806eb77d9d7a1319718c00896e2c6be87834b5
chorimgenius <artyum15@kakao.com>
2022-10-19 10:56:27 +0900

    [추가]사용자 중복 확인  추가

--- user/views.py
+++ user/views.py
@@ -24,6 +24,14 @@ def signup(request):
             if username == '' or password == '':
             # 사용자 저장을 위한 username과 password가 필수라는 것을 얘기 해 줍니다.
                 return render(request, 'user/signup.html', {'error': '사용자 이름과 패스워드는 필수 값 입니다'})
+        
+        exist_user = get_user_model().objects.filter(username=username)
+        if exist_user:
+            return render(request, 'user/signup.html',
+                        {'error': '사용자가 이미 존재합니다.'})  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+        else:
+            User.objects.create_user(username=username, password=password)
+            return redirect('/signin')  # 회원가입이 완료되었으므로 로그인 페이지로 이동
 
 def signin(request):
     if request.method == "GET":
