c8a59cb90d7edbfe6ef07109b492e22f635056cd
naerumii <github>
2022-10-19 19:55:26 +0900

    fix : django 머지 후 오류 발생되는 불필요 코드 정리 1. 로그인, 회원가입 기능 test용으로 만들었던 home.html 파일삭제 2. user/views.py 에서 home모델 삭제 3. home 삭제에 따른 urls.py 수정 4. tweet/views.py img_list모델 수정

--- user/views.py
+++ user/views.py
@@ -59,9 +59,4 @@ def logout(request):
     auth.logout(request)
     return redirect('/')
 
-def home(request):
-    user = request.user.is_authenticated
-    if user:
-        return render(request,'home.html')
-    else:
-        return redirect('/signin')
+
